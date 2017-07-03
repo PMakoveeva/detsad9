6bb7b9e87611f1108a47fe0dbae5ae3ce7111513
Ristee <rristee@gmail.com>
2017-07-03 14:39:16 +0500

    Update SiteController.php

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -147,33 +147,8 @@ class SiteController extends Controller
 
 
     public function actionBlog(){
-        date_default_timezone_set('Asia/Yekaterinburg');
-
-        $hour=date('G');
-
-        $hour = (isset($_GET['hour']))?$_GET['hour']:$hour;
-
-        if($hour>=23 || $hour<5 && $hour>=0){
-            $css['colour_line_top']='night_line_top';
-            $css['bg_footer']='bg_footer_night';
-            $css['header']='header_clouds_night';
-        }
-        if($hour>5 && $hour<13){
-            $css['colour_line_top']='morning_line_top';
-            $css['bg_footer']='bg_footer_morning';
-            $css['header']='header_clouds_mo';
-        }
-        if($hour>=13 && $hour<17){
-            $css['colour_line_top']='day_line_top';
-            $css['bg_footer']='bg_footer_day';
-            $css['header']='header_clouds_day';
-        }
-        if($hour>=17 && $hour<23){
-            $css['colour_line_top']='evening_line_top';
-            $css['bg_footer']='bg_footer_evening';
-            $css['header']='header_clouds_evening';
-        }
-        return $this->render('blog', $css);
+        
+        return $this->render('blog');
     }
 
 
